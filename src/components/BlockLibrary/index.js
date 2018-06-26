7079b52a467246f40d8f568973fbb85685d114cd
Courtney Myers <courtney.aaron.myers@gmail.com>
2018-06-26 16:33:30 -0400

    Add remove buttons to BlockLibrary blocks

--- src/components/BlockLibrary/index.js
+++ src/components/BlockLibrary/index.js
@@ -2,20 +2,39 @@
 
 import React from 'react';
 import styled from 'styled-components';
+// components
+import BlockButton from 'components/BlockButton';
 
 // --- styled components
 const Container = styled.div`
   /* */
 `;
 
+const RemoveButton = styled(BlockButton)`
+  /* */
+`;
+
 const Block = styled.div`
+  display: flex;
   margin-top: 0.5rem;
-  padding: 0.5rem;
   border: 1px solid #ccbee4;
   background-color: white;
 `;
 
-const Text = styled.p`
+const Handle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-basis: 1.75rem;
+  background-color: #ccbee4;
+`;
+
+const Text = styled.div`
+  flex-grow: 1;
+  padding: 0.5rem;
+`;
+
+const Paragraph = styled.p`
   margin-top: 0.5rem;
   margin-bottom: 0;
   font-size: 0.875rem;
@@ -25,7 +44,7 @@ const Text = styled.p`
   }
 `;
 
-const Title = Text.extend`
+const Title = Paragraph.extend`
   font-weight: bold;
   color: #360a80;
 `;
@@ -44,7 +63,18 @@ const BlockLibrary = (props: Props) => (
   <Container {...props}>
     {props.blocks.map((block) => (
       <Block key={block.time}>
-        <Title>{block.title}</Title>
+        <Handle>
+          <RemoveButton
+            text="–"
+            href=""
+            onClick={(ev) => {
+              ev.preventDefault();
+            }}
+          />
+        </Handle>
+        <Text>
+          <Title>{block.title}</Title>
+        </Text>
       </Block>
     ))}
   </Container>
