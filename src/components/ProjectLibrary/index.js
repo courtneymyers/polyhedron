4430189531db96ddda2fd354a26836703f547cb1
Courtney Myers <courtney.aaron.myers@gmail.com>
2018-07-02 14:42:45 -0400

    Update ProjectLibrary to use LibraryItem component

--- src/components/ProjectLibrary/index.js
+++ src/components/ProjectLibrary/index.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import styled from 'styled-components';
 // components
-import BlockButton from 'components/BlockButton';
+import LibraryItem from 'components/LibraryItem';
 // types
 import type { ArticleProps } from 'contexts/articles';
 
@@ -11,54 +11,6 @@ import type { ArticleProps } from 'contexts/articles';
 const Container = styled.div`
   /* */
 `;
-
-const RemoveButton = styled(BlockButton)`
-  /* */
-`;
-
-/* -------------------------------------------------------------------------- */
-/* TODO: DRY repeated code below (copied from components/BlockLibrary) */
-/* -------------------------------------------------------------------------- */
-const Article = styled.div`
-  display: flex;
-  margin-top: 0.5rem;
-  border: 1px solid #ccbee4;
-  border-radius: 3px;
-  background-color: white;
-`;
-
-const Handle = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-basis: 1.75rem;
-  background-color: #ccbee4;
-`;
-
-const Text = styled.div`
-  flex-grow: 1;
-  padding: 0.5rem;
-  user-select: none;
-  cursor: move;
-`;
-
-const Paragraph = styled.p`
-  margin-top: 0.5rem;
-  margin-bottom: 0;
-  font-size: 0.875rem;
-
-  :first-of-type {
-    margin-top: 0;
-  }
-`;
-
-const Title = Paragraph.extend`
-  font-weight: bold;
-  color: #360a80;
-`;
-/* -------------------------------------------------------------------------- */
-/* TODO: DRY repeated code above (copied from components/BlockLibrary) */
-/* -------------------------------------------------------------------------- */
 
 // --- components
 type Props = {
@@ -70,22 +22,14 @@ type Props = {
 const ProjectLibrary = (props: Props) => (
   <Container {...props}>
     {props.articles.map((article) => (
-      <Article key={article.id}>
-        <Handle>
-          <RemoveButton
-            text="–"
-            href=""
-            title="Remove Article"
-            onClick={(ev) => {
-              ev.preventDefault();
-              props.removeArticle(article.id);
-            }}
-          />
-        </Handle>
-        <Text>
-          <Title>{article.title === '' ? '\u00A0' : article.title}</Title>
-        </Text>
-      </Article>
+      <LibraryItem
+        key={article.id}
+        id={article.id}
+        label="Article"
+        title={article.title}
+        desc={article.desc}
+        removeItem={props.removeArticle}
+      />
     ))}
   </Container>
 );
