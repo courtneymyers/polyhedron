4d7391b3bccc18e2da2122ddefe9f235510815a0
Courtney Myers <courtney.aaron.myers@gmail.com>
2018-10-10 21:50:36 -0400

    Add flow type definitions for jest, and update index test to use correct component

--- src/index.test.js
+++ src/index.test.js
@@ -3,10 +3,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // components
-import AppDND from 'components/AppDND';
+import App from 'components/App';
 
 it('renders without crashing', () => {
   const rootElement: any = document.createElement('div');
-  ReactDOM.render(<AppDND />, rootElement);
+  ReactDOM.render(<App db="firebase" />, rootElement);
   ReactDOM.unmountComponentAtNode(rootElement);
 });
