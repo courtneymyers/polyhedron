b3ff6f7b7441f67f4f87705e1bdf33f6c16eb5bd
Courtney Myers <courtney.aaron.myers@gmail.com>
2018-10-28 08:27:04 -0400

    Move firebase project and messaging ID into env variables

--- src/databases/firebase.js
+++ src/databases/firebase.js
@@ -1,12 +1,20 @@
+// @flow
+
 import firebase from 'firebase';
 
+const firebaseApiKey = process.env.REACT_APP_FIREBASE_API_KEY;
+const firebaseProjectId = process.env.REACT_APP_FIREBASE_PROJECT_ID;
+const firebaseMessagingId = process.env.REACT_APP_FIREBASE_MESSAGING_ID;
+
+if (!firebaseProjectId) throw new Error('Firebase Project ID error.');
+
 firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: 'polyhedron-db.firebaseapp.com',
-  databaseURL: 'https://polyhedron-db.firebaseio.com',
-  projectId: 'polyhedron-db',
-  storageBucket: 'polyhedron-db.appspot.com',
-  messagingSenderId: '687411267088',
+  apiKey: firebaseApiKey,
+  authDomain: `${firebaseProjectId}.firebaseapp.com`,
+  databaseURL: `https://${firebaseProjectId}.firebaseio.com`,
+  projectId: `${firebaseProjectId}`,
+  storageBucket: `${firebaseProjectId}.appspot.com`,
+  messagingSenderId: firebaseMessagingId,
 });
 
 export default firebase;
