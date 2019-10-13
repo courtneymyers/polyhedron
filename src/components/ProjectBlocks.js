e94503e534f585e557970361cc11a810e35d6dfc
Courtney Myers <courtney.aaron.myers@gmail.com>
2019-10-13 07:08:07 -0400

    Convert ProjectBlocks component to a function component

--- src/components/ProjectBlocks.js
+++ src/components/ProjectBlocks.js
@@ -22,53 +22,47 @@ type Props = {
   projectId: string,
   blocks: Array<BlockProps>,
 };
-type State = {};
 
-class ProjectBlocks extends React.Component<Props, State> {
-  static contextType = ProjectsContext;
+function ProjectBlocks({ projectId, blocks }: Props) {
+  const { removeBlockIdFromProject } = React.useContext(ProjectsContext);
 
-  render() {
-    const { removeBlockIdFromProject } = this.context;
-    const { blocks, projectId } = this.props;
-
-    return (
-      <Droppable droppableId={`project-blocks`}>
-        {(provided, snapshot) => (
-          <DropContainer
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-            isDraggingOver={snapshot.isDraggingOver} // custom prop for styling
-          >
-            {blocks.map((block, index) => (
-              <Draggable
-                key={block.id}
-                draggableId={`${projectId}-${block.id}`}
-                index={index}
-              >
-                {(provided, snapshot) => (
-                  <DragContainer
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                  >
-                    <Block
-                      id={block.id}
-                      meta={block.meta}
-                      body={block.body}
-                      removeBlock={(blockId) =>
-                        removeBlockIdFromProject(projectId, blockId)
-                      }
-                    />
-                  </DragContainer>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </DropContainer>
-        )}
-      </Droppable>
-    );
-  }
+  return (
+    <Droppable droppableId={`project-blocks`}>
+      {(provided, snapshot) => (
+        <DropContainer
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+          isDraggingOver={snapshot.isDraggingOver} // custom prop for styling
+        >
+          {blocks.map((block, index) => (
+            <Draggable
+              key={block.id}
+              draggableId={`${projectId}-${block.id}`}
+              index={index}
+            >
+              {(provided, snapshot) => (
+                <DragContainer
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                >
+                  <Block
+                    id={block.id}
+                    meta={block.meta}
+                    body={block.body}
+                    removeBlock={(blockId) =>
+                      removeBlockIdFromProject(projectId, blockId)
+                    }
+                  />
+                </DragContainer>
+              )}
+            </Draggable>
+          ))}
+          {provided.placeholder}
+        </DropContainer>
+      )}
+    </Droppable>
+  );
 }
 
 export default ProjectBlocks;
